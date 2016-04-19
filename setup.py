666527a0eeb40a026f96fe815b242d4668a54a09
Julien Palard <julien@palard.fr>
2016-04-19 19:25:32 +0200

    setup.py: Add long description.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='asynczip',
       version='1.0',
       description='Asynchronous `zip` like aggregator for `async for`',
+      long_description=open('README.md', 'rt').read(),
       author='Julien Palard',
       author_email='julien@palard.fr',
       url='https://github.com/JulienPalard/asynczip',
