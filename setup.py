710955f779586c10e5b2bc9c968b9614c2411356
Julien Palard <julien@palard.fr>
2016-04-20 16:36:56 +0200

    Try to make pypi understand I have a README.rst.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='asynczip',
-      version='1.0.6',
+      version='1.0.7',
       description='Asynchronous `zip` like aggregator for `async for`',
       long_description=open('README.rst', 'rt').read(),
       author='Julien Palard',
       author_email='julien@palard.fr',
       keywords=['asyncio', 'zip', 'select', 'async for', 'async'],
       url='https://github.com/JulienPalard/asynczip',
-      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0.6',
+      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0.7',
       py_modules=['asynczip'],
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
