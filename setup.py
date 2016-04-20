c4ecce1ac45425766ff10a9893166a83fec17770
Julien Palard <julien@palard.fr>
2016-04-20 16:07:44 +0200

    Bump version, with the link...

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='asynczip',
-      version='1.0.2',
+      version='1.0.3',
       description='Asynchronous `zip` like aggregator for `async for`',
       long_description=open('README.rst', 'rt').read(),
       author='Julien Palard',
       author_email='julien@palard.fr',
       keywords=['asyncio', 'zip', 'select', 'async for', 'async'],
       url='https://github.com/JulienPalard/asynczip',
-      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0',
+      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0.3',
       py_modules='asynczip',
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
