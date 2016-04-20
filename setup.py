8435008c0d3276e31c159f71c8beb2174ed0ea0e
Julien Palard <julien@palard.fr>
2016-04-20 16:17:32 +0200

    FIX: py_modules is a list.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from distutils.core import setup
 
 setup(name='asynczip',
-      version='1.0.4',
+      version='1.0.5',
       description='Asynchronous `zip` like aggregator for `async for`',
       long_description=open('README.txt', 'rt').read(),
       author='Julien Palard',
       author_email='julien@palard.fr',
       keywords=['asyncio', 'zip', 'select', 'async for', 'async'],
       url='https://github.com/JulienPalard/asynczip',
-      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0.4',
-      py_modules='asynczip',
+      download_url='https://github.com/julienpalard/AsyncZip/tarball/1.0.5',
+      py_modules=['asynczip'],
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License']
